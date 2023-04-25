581c393e0b791bd5c4d4c081a35236a6d301e3e0
Cecilia Iribarren <cecirib@gmail.com>
2023-04-25 10:18:03 -0300

    Add: agregamos clases de exception más específicas

--- Python/Excepciones/Leccion 1/manejoExcepciones.py
+++ Python/Excepciones/Leccion 1/manejoExcepciones.py
@@ -1,6 +1,13 @@
 try:
     10/0
     #10/0 siempre dará error
-except Exception as e:
-    print(f'Ha ocurrido un error: {e}')
+
+# Para saber que tipo de excepción está llamando
+except TypeError as e:
+    print(f'TypeError - Ocurrió un error: {type(e)}')
+except ZeroDivisionError as e:
+    print(f'ZeroDivisionError - Ocurrió un error: {type(e)}')
+# La clase mas genérica se coloca al final del bloque try
+except Exception as e:  # Clase padre Exception
+    print(f'Ha ocurrido un error: {e} - Exception')
     #Output: Ha ocurrido un error: division by zero
