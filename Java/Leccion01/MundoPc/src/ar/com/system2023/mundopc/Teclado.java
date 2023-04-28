9e7b2d96eae73d582137c70155d1416e0ef3049f
GuillermoEQ <qguillermoe@gmail.com>
2023-04-27 23:10:15 -0300

    add: Se construyen las Clases Hijas Ratón y Teclado

---
+++ Java/Leccion01/MundoPc/src/ar/com/system2023/mundopc/Teclado.java
@@ -0,0 +1,16 @@
+package ar.com.system2023.mundopc;
+
+public class Teclado extends DispositivoEntrada{
+     private final int idTeclado;
+    private static int contadorTeclado;
+    
+    public Teclado(String  tipoEntrada, String marca){
+        super(tipoEntrada, marca);
+        this.idTeclado = ++Teclado.contadorTeclado;
+    }
+
+    @Override
+    public String toString() {
+        return "Teclado{" + "idTeclado=" + idTeclado + ", " + super.toString() + '}';
+    }
+}
