59ef2aaf1caebc1ff4a1a21d21c289a2684b1c0f
GiuliGaffoglio <111721673+GiuGaffoglio@users.noreply.github.com>
2023-04-30 01:27:46 -0300

    Bloque else y finally
     Bloques else y finally al manejar excepciones

--- Python/Excepciones/Leccion 1/manejoExcepciones.py
+++ Python/Excepciones/Leccion 1/manejoExcepciones.py
@@ -1,13 +1,15 @@
-try:
-    10/0
-    #10/0 siempre dará error
+resultado = None
 
-# Para saber que tipo de excepción está llamando
+try:
+    a = int(input('Digite el primer numero: ')
+    b = int(input('Digite el segundo numero: ')
+    resultado = a / b # modificamso
 except TypeError as e:
     print(f'TypeError - Ocurrió un error: {type(e)}')
 except ZeroDivisionError as e:
     print(f'ZeroDivisionError - Ocurrió un error: {type(e)}')
-# La clase mas genérica se coloca al final del bloque try
-except Exception as e:  # Clase padre Exception
-    print(f'Ha ocurrido un error: {e} - Exception')
-    #Output: Ha ocurrido un error: division by zero
+except Exception as e:  
+    print(f'Exception - Ocurrio un error: {type(e)}') 
+           
+print(f'El resultado es: {resultado}')
+print('seguimos...')            
