eed719cc8ac37db207962f19f3b312a8f793382c
Agustin Vera <ronedev25@gmail.com>
2023-05-06 23:27:21 -0300

    add: recorremos un arreglo mediante un bucle for

--- JavaScript/TSLeccion02/02-03-arreglos.js
+++ JavaScript/TSLeccion02/02-03-arreglos.js
@@ -1,3 +1,13 @@
+
+// Creación de Array o arreglos
 //let autos = new Array('Ferrari','Renault','BMW'); esta es la sintaxis vieja
 const autos = ['Ferrari','Renault','BMW'];
 console.log(autos); 
+
+//Recorremos los elementos de un arreglo
+console.log(autos[0])
+console.log(autos[2])
+
+for (let i = 0; i < autos.length; i++) {
+    console.log(i + ': ' + autos[i])
+}
