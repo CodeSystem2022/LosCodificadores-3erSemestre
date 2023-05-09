d0164da0adfff0501821bae47907aec2267fc70d
GiuliGaffoglio <111721673+GiuGaffoglio@users.noreply.github.com>
2023-05-08 22:05:26 -0300

    Modificamos los elementos
    Modificamos los elementos

--- JavaScript/TSLeccion02/02-03-arreglos.js
+++ JavaScript/TSLeccion02/02-03-arreglos.js
@@ -11,3 +11,19 @@ console.log(autos[2])
 for (let i = 0; i < autos.length; i++) {
     console.log(i + ': ' + autos[i])
 }
+
+// Modificamos los elementos del arreglo
+autos[1] = 'Volvo';
+console.log(autos[1]); 
+
+//Agregamos nuevos valores al arreglo
+autos.push('Audi'); // Agregamos el elemento al final del arreglo
+console.log(autos);
+
+// Otras formas de agregar elementos al arreglo
+autos[autos.length] = 'Porche';
+console.log(autos);
+
+//Tercera forma de agreagr elementos teniendo CUIDADO
+autos[6] = 'Renault';
+console.log(autos);
