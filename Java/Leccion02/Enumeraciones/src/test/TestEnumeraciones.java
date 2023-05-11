85115641671a37ab0e29660e0cbba85ff6d2dcb1
Giuliana <giuliana9566@gmail.com>
2023-05-11 11:43:07 -0300

    Pruebas de enum. con la creacion de enum. Continentes

--- Java/Leccion02/Enumeraciones/src/test/TestEnumeraciones.java
+++ Java/Leccion02/Enumeraciones/src/test/TestEnumeraciones.java
@@ -4,6 +4,33 @@ import enumeraciones.Dias;
 
 public class TestEnumeraciones {
     public static void main(String[] args) {
-        System.out.println("Dia 1: " + Dias.LUNES);
+       // System.out.println("Dia 1: " + Dias.LUNES);
+       
+       
+        System.out.println("Continente Nro. 4:"+Continentes.AMERICA);
+        System.out.println("Nro. de paises en el 4to. continente: "
+                +Continentes.AMERICA.getPaises());
+        System.out.println("Nro. de habitantes en el 4to. continente: "
+                +Continentes.AMERICA.getHabitantes());
+        System.out.println("Continente Nro. 1:"+Continentes.AFRICA);
+        System.out.println("Nro. de paises en el 1er. continente: "
+                +Continentes.AFRICA.getPaises());
+        System.out.println("Nro. de habitantes en el 1er. continente: "
+                +Continentes.AFRICA.getHabitantes());
+        System.out.println("Continente Nro. 2:"+Continentes.EUROPA);
+        System.out.println("Nro. de paises en el 2do. continente: "
+                +Continentes.EUROPA.getPaises());
+        System.out.println("Nro. de habitantes en el 2do. continente: "
+                +Continentes.EUROPA.getHabitantes());
+        System.out.println("Continente Nro. 3:"+Continentes.ASIA);
+        System.out.println("Nro. de paises en el 3ro. continente: "
+                +Continentes.ASIA.getPaises());
+        System.out.println("Nro. de habitantes en el 3ro. continente: "
+                +Continentes.ASIA.getHabitantes());
+        System.out.println("Continente Nro. 5:"+Continentes.OCEANIA);
+        System.out.println("Nro. de paises en el 5to. continente: "
+                +Continentes.OCEANIA.getPaises());
+        System.out.println("Nro. de habitantes en el 5to. continente: "
+                +Continentes.OCEANIA.getHabitantes());
     }
 }
