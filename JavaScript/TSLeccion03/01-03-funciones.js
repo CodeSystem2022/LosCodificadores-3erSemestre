9ba5bf21caf7def8889fa10027e8c7269c51aefd
Agustin Vera <ronedev25@gmail.com>
2023-05-10 23:35:03 -0300

    add: Paso por valor en funciones

--- JavaScript/TSLeccion03/01-03-funciones.js
+++ JavaScript/TSLeccion03/01-03-funciones.js
@@ -10,3 +10,61 @@ function miFuncion(a, b){
 
 let resultado = miFuncion(6, 7);
 console.log(resultado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Tipos primitivos
+let k = 10;
+function cambiarValor(a){ //Paso por valor, la variable pasada a la funcion no sufre ningun cambio, como si se pasara unicamente una copia
+    a = 20;
+}
+
+cambiarValor(k)
+console.log(k)
