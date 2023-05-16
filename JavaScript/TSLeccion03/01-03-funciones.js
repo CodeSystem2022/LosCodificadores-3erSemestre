cd813bb2da3a69489dea38fb37c26b21d279f6a6
Giuliana <giuliana9566@gmail.com>
2023-05-16 18:11:49 -0300

    Argumentos y parametros

--- JavaScript/TSLeccion03/01-03-funciones.js
+++ JavaScript/TSLeccion03/01-03-funciones.js
@@ -39,14 +39,14 @@ console.log(resultado)
 
 
 
-
-
-
-
-
-
-
-
+// Funcion de tipo expresion
+let sumar = function (a = 4, b = 8){
+    console.log(arguments[0]); // muestra el parametro de: a
+    console.log(arguments[1]); // muestra el parametro de: b
+    return a + b + arguments[2];
+}
+resultado = sumar(3, 2, 9);
+console.log(resultado);
 
 
 //Sumar todos los argumentos
