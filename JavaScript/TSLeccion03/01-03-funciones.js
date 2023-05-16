9a8d734342c4f00d5c5f9adf94d38862e7db338f
Cecilia Iribarren <cecirib@gmail.com>
2023-05-16 15:35:12 -0300

    Add: Paso por referencia

--- JavaScript/TSLeccion03/01-03-funciones.js
+++ JavaScript/TSLeccion03/01-03-funciones.js
@@ -26,7 +26,19 @@ console.log(resultado);
 
 
 
+//Video 10: Paso por referencia
+const persona = {
+    nombre: 'Juan',
+    apellido: 'Lepez'
+}
+console.log(persona);
+function cambiarValorObjeto(p1){
+    p1.nombre = 'Ignacio';
+    p1.apellido = 'Perez';
+}
 
+cambiarValorObjeto(persona);
+console.log(persona);
 
 
 
