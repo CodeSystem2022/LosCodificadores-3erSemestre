6d82f521c5e92ca1eb101dd3286d42c59dfb14cc
Giuliana <giuliana9566@gmail.com>
2023-05-16 17:16:09 -0300

    Concepto hoisting

--- JavaScript/TSLeccion03/01-03-funciones.js
+++ JavaScript/TSLeccion03/01-03-funciones.js
@@ -49,7 +49,16 @@ console.log(resultado)
 
 
 
-
+//Sumar todos los argumentos
+let respuesta = sumarTodo (5, 4, 13, 10, 9);
+console.log(respuesta);
+function sumarTodo(){
+    let suma = 0;
+    for(let i = 0; i < arguments.length; i++){
+        suma += arguments[i]; // arguments es para arreglos
+    }
+    return suma;
+}
 
 
 
