3d608813723b3d8c6b2e147aafa210a1d29b4bec
Cecilia Iribarren <cecirib@gmail.com>
2023-05-17 01:25:22 -0300

    Add: Modificadores de acceso protected

--- Java/Leccion03/ModificadoresAcceso/src/paquete2/Clase3.java
+++ Java/Leccion03/ModificadoresAcceso/src/paquete2/Clase3.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package paquete2;
 
-/**
- *
- * @author torto
- */
-public class Clase3 {
-    
+import paquete1.Clase1;
+
+
+public class Clase3 extends Clase1{
+    public Clase3(){
+        super("protected");
+        this.atributoProtected = "Accedemos desde la clase hija";
+        System.out.println("AtributoProtected = " + this.atributoProtected);
+        this.atributoPublic = "es totalmente publico";
+    }
 }
--- Java/Leccion03/ModificadoresAcceso/src/test/TestModificadoresAcceso.java
+++ Java/Leccion03/ModificadoresAcceso/src/test/TestModificadoresAcceso.java
@@ -1,8 +1,14 @@
 
 package test;
 
+import paquete2.Clase3;
+
 
 
 public class TestModificadoresAcceso {
     
+    //Creamos objeto de la Clase hija 3
+   Clase3 claseHija = new Clase3();
+    System.out.println ("claseHija = " + claseHija);
+
 }
