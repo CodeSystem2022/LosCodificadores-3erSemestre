36e67486755a69582197528cdfe527e945249643
GuillermoEQ <qguillermoe@gmail.com>
2023-05-17 20:13:17 -0300

    add: Se realizaron modificaciones al código

--- Python/Archivos/Leccion02/leer_archivo.py
+++ Python/Archivos/Leccion02/leer_archivo.py
@@ -5,20 +5,30 @@
     y en los mencionados en "manejo_archivos" y (por ende) en "prueba.txt".
 """
 
-archivo = open('prueba.txt','r', encoding='utf8')# las letas son: 'r' read, 'a' append, 'w' write, 'x'
+archivo = open('prueba.txt','r', encoding='utf8')
 #print(archivo.read())
-#print(archivo.read(5))# leemos esa cantidad de letras
-#print(archivo.readline())#lee la primera linea
+#print(archivo.read(15))                # Se lee esa cantidad de letras
+#print(archivo.read(5))                 # Continúa la lectura desde la línea anterior
+#print(archivo.readline())              # Se lee la primera línea
+#print(archivo.readline())              # Otra
 
-# vamos a iterar el archivo, cada una de las lineas
+# Iterar el archivo, cada una de las líneas:
+    
 #for linea in archivo:
-   # print(linea) # iteramos todos los elementos del archivo
-   # print(archivo.readlines()) accedemos al archivo como si fuera una lista
-    #print(archivo.readlines()[11])
-# anexamos informacion, copiamos a otro
+   # print(linea)                    
+   # print(archivo.readlines()) 
+   # print(archivo.readlines()[11])     # Se accede al archivo como si fuera una lista
+
+# Anexo información, y copia a otro:
+    
 archivo2 = open('copia.txt', 'a', encoding='utf8')
 archivo2.write(archivo.read())
 archivo.close() # cerramos el primer archivo
 archivo2.close() # cerramos el segundo archivo
 
 print('se ha terminado el proceso de leer y copiar archivos')
+
+
+# Con 'a', si se ejecuta dos veces, se anexa también dos veces la información.
+# Si se cambia a 'w' y se ejecuta, vuelve a tenerse una vez lo copiado,
+# ya que se crea un nuevo archivo reemplazándose el contenido.
