152742c5d171c9bf602979a5c53286d51b4f4672
Cecilia Iribarren <cecirib@gmail.com>
2023-05-17 00:53:41 -0300

    Add: creamos constructor y método protected en la Clase1

--- Java/Leccion03/ModificadoresAcceso/src/paquete1/Clase1.java
+++ Java/Leccion03/ModificadoresAcceso/src/paquete1/Clase1.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package paquete1;
 
-/**
- *
- * @author torto
- */
+
 public class Clase1 {
     
+    protected String atributoProtected = "Valor atributo protected";//Viene del video 7
+    /*
+    agregar código videos 5 y 6
+    
+    */
+    
+    
+   //Constructor protected
+    protected Clase1(String atributoPublic){
+        System.out.println("Constructor protected");
+    }
+    /*
+    
+    Agregar codigo videos 5 y 6
+    
+    */
+    //Metodo protected
+    protected void metodoProtected(){
+        System.out.println("Método protected");
+    }
 }
