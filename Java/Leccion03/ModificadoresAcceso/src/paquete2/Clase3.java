d611af9041ded17579579b7499122b0884cb76f4
Cecilia Iribarren <cecirib@gmail.com>
2023-05-17 01:08:27 -0300

    Add: paquete2 y Clase3

---
+++ Java/Leccion03/ModificadoresAcceso/src/paquete2/Clase3.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete2;
+
+/**
+ *
+ * @author torto
+ */
+public class Clase3 {
+    
+}
