3275191d4d58d0ebddf9e358568214ef01c20b76
Nicolas Aranibar <aranibarnicolas596@gmail.com>
2023-05-18 23:49:06 -0300

    Funciones tipo self y invoking

--- JavaScript/TSLeccion03/01-03-funciones.js
+++ JavaScript/TSLeccion03/01-03-funciones.js
@@ -17,6 +17,10 @@ let x = function(a,b){return a+b}; //necesita cierre con punto y coma ;
 resultado = x(5,6); //al llamarla se pone la variable y parentesis
 console.log(resultado);
 
+//Funciones de tipo self y invoking
+(function(a, b){
+console.log('Ejecutando la funcion: '+ (a + b));
+})(9, 6);
 
 
 //Video 10: Paso por referencia
