250e6e1b4289e2138ec8c4cc9d5c87253103bd0e
Agustin Vera <ronedev25@gmail.com>
2023-05-22 00:34:36 -0300

    add: Distintas formas de imprimir un objeto

--- JavaScript/TSLeccion04/01-04-objetos.js
+++ JavaScript/TSLeccion04/01-04-objetos.js
@@ -14,3 +14,46 @@ console.log(persona.apellido);
 console.log(persona.email);
 console.log(persona.edad);
 console.log(persona)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Distintas formas de imprimir un objeto
+//Numero 1: la mas sencilla, concatenar cada valor de cada propiedad
+console.log("Distintas formas de imprimir un objeto: forma 1")
+console.log(persona.nombre + ', ' + persona.apellido)
+
+//Numero 2: A traves del ciclo for in
+console.log("Distintas formas de imprimir un objeto: forma 2")
+for(nombrePropiedad in persona){
+    console.log(persona[nombrePropiedad])
+}
+
+//Numero 3: La funcion Object.value()
+console.log("Distintas formas de imprimir un objeto: forma 3")
+let personaArray = Object.values(persona);
+console.log(personaArray)
+
+//Numero 4: Usamos el metodo JSON.stringify
+console.log("Distintas formas de imprimir un objeto: forma 4")
+let personaString = JSON.stringify(persona);
+console.log(personaString)
