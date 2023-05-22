b6cd9c0fcc6af3197a425f3f08eeb80510b209dd
Agustin Vera <ronedev25@gmail.com>
2023-05-22 00:02:50 -0300

    add: Creacion del archivo de JavaScript correspondiente a la clase 4 y del objeto persona

---
+++ JavaScript/TSLeccion04/01-04-objetos.js
@@ -0,0 +1,12 @@
+let x = 10; //Variable tipo primitiva
+console.log(x.length);
+
+//Objeto
+let persona = {
+    nombre: "Carlos",
+    apellido: "Gil",
+    email: "cgil@gmail.com",
+    edad: 30
+}
+
+console.log(persona.nombre);
