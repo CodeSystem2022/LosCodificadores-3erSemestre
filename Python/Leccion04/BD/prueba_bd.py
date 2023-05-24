ed0362435f9619d333f636bd88a185194adb2019
Giuliana <giuliana9566@gmail.com>
2023-05-24 12:31:03 -0300

    Funciones fetchone en psycopg2

--- Python/Leccion04/BD/prueba_bd.py
+++ Python/Leccion04/BD/prueba_bd.py
@@ -6,23 +6,17 @@
 # Click en terminal
 # Escribimos pip install psycopg2 + enter
 
-import psycopg2 #Esto es para conectarnos a postgres
+import psycopg2 # Esto es para poder conectarnos a Postgre
 
-conexion = psycopg2.connect(
-    user = 'postgres',
-    password = 'admin',
-    host = '127.0.0.1',
-    port = '5432',
-    database = 'test_bd'
-)
-
+conexion = psycopg2.connect(user= 'postgres', password= 'admin', host= '127.0.01', port= '5432', database= 'test_bd')
 try:
     with conexion:
-        with conexion.cursor() as cursor:
-            sentencia = 'SELECT * FROM persona'
-            cursor.execute(sentencia) #De esta manera ejecutamos la sentencia
-            registros = cursor.fetchall() #Recuperamos todos los registros que seran una lista
-            print(registros)
+        with conexion.cursor()as cursor: 
+        sentencia = 'SELECT * FROM persona WHERE id_persona = %s' # Placeholder
+        id_persona = input('Digite u numero para el id_persona: ')
+        cursor.execute(sentencia, (id_persona,))# De esta manera ejecutamos la sentencia
+        registros = cursor.fetchone() # Recuperamos todos los registros que seran una lista
+        print(registros)
 except Exception as e:
     print(f'Ocurrio un error: {e}')
 finally:
