dfa29aa84a534767be3d77d483bf8cfb9a1a74a3
Agustin Vera <ronedev25@gmail.com>
2023-05-24 13:31:32 -0300

    add: funcionamiento del metodo call

--- JavaScript/TSLeccion04/01-04-objetos.js
+++ JavaScript/TSLeccion04/01-04-objetos.js
@@ -121,3 +121,30 @@ let miArreglo2 = []; //Sintaxis recomendada
 let miFuncion1 = new function(){}; //Todo despues de new es considerado objeto
 //Caso function 2
 let miFuncion2 = function(){}; //Sintaxis simplificada y recomendada
+
+
+
+
+
+
+
+
+
+
+
+//Uso de call
+let persona4 = {
+    nombre: "Juan",
+    apellido: "Perez",
+    nombreCompleto2: function(titulo, telefono){
+        return titulo + ': ' + this.nombre + ' ' + this.apellido + ' ' + telefono
+    }
+}
+
+let persona5 = {
+    nombre: "Carlos",
+    apellido: "Lara"
+}
+
+console.log(persona4.nombreCompleto2('Lic', '1321312312312'))
+console.log(persona4.nombreCompleto2.call(persona5, 'Ing', '123124543534'))
