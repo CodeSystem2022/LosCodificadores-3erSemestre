958a1d720414cac0106f3ba45b207f55695b1219
Agustin Vera <ronedev25@gmail.com>
2023-05-24 10:40:06 -0300

    add: carpeta leccion05, clases Empleado y Escritor y enumeracion TipoEscritura

---
+++ Java/Leccion05/ConversionObjetos/src/domain/Empleado.java
@@ -0,0 +1,36 @@
+package domain;
+
+public class Empleado {
+    public String nombre;
+    public double sueldo;
+    
+    public Empleado(String nombre, double sueldo){
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" + "nombre=" + nombre + ", sueldo=" + sueldo + '}';
+    }
+    
+    public String obtenerDetalle(){
+        return "Nombre: " + this.nombre + ", Sueldo: $" + this.sueldo;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo() {
+        return this.sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+}
