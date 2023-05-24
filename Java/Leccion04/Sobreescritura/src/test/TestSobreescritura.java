c54bee870b2e0c1ac27bac4566fab6feb8ccce7a
Cecilia Iribarren <cecirib@gmail.com>
2023-05-24 12:12:39 -0300

    Add: Polimorfismo parte 1

--- Java/Leccion04/Sobreescritura/src/test/TestSobreescritura.java
+++ Java/Leccion04/Sobreescritura/src/test/TestSobreescritura.java
@@ -1,11 +1,19 @@
 package test;
 
-import domain.Gerente;
+import domain.*;
+
 
 
 public class TestSobreescritura {
     public static void main(String[] args) {
-        Gerente gerente1 = new Gerente("Jose", 5000, "Sistemas");
-        System.out.println("gerente1 = " + gerente1.obtenerDetalles());
+         Empleado empleado1 = new Empleado("Juan", 10000);
+         imprimir(empleado1);
+         //System.out.println("empleado1 = " + empleado1.obtenerDetalles());
+         Gerente gerente1 = new Gerente("Jose", 5000, "Sistemas");
+         imprimir(gerente1);
+         //System.out.println("gerente1 = " + gerente1.obtenerDetalles());
+    }
+    public static void imprimir(Empleado empleado){
+        System.out.println("empleado = " + empleado.obtenerDetalles());
     }
 }
