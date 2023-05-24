f00aecfcdd9281d3b478c9316d20b36abbb1bb2b
Agustin Vera <ronedev25@gmail.com>
2023-05-24 11:05:56 -0300

    add: Distintas formas de crear objetos

--- JavaScript/TSLeccion04/01-04-objetos.js
+++ JavaScript/TSLeccion04/01-04-objetos.js
@@ -57,3 +57,67 @@ console.log(personaArray)
 console.log("Distintas formas de imprimir un objeto: forma 4")
 let personaString = JSON.stringify(persona);
 console.log(personaString)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Diferentes formas de crear objetos
+//Caso numero 1
+let miObjeto = new Object() //Esta es una opcion formal
+//Caso numero 2
+let miObjeto2 = {}; //Esta es una opcion breve y recomendada
+
+//Caso string 1
+let miCadena1 = new String('Hola');//Sintaxis formal
+//Caso string 2 
+let miCadena2 = 'Hola' //Esta es la sintaxis simplificada y recomendada
+
+//Caso con numeros 1
+let miNumero = new Number(1); //Sintaxis formal no recomendable
+//Caso con numeros 2
+let miNumero2 = 1; //Sintaxis recomendada
+
+//Caso boolean 1
+let miBoolean1 = new Boolean(false); //Formal
+//Caso boolean 2
+let miBoolean2 = false; //Sintaxis recomendada
+
+//Caso arreglos 1
+let miArreglo1 = new Array(); //Formal
+//Caso arreglos 2
+let miArreglo2 = []; //Sintaxis recomendada
+
+//Caso function 1
+let miFuncion1 = new function(){}; //Todo despues de new es considerado objeto
+//Caso function 2
+let miFuncion2 = function(){}; //Sintaxis simplificada y recomendada
