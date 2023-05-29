84ce516513ee78590786cd0f19813575676a98f0
Agustin Vera <ronedev25@gmail.com>
2023-05-29 14:22:40 -0300

    fix: 01-04-objetos.js conflictos

--- JavaScript/TSLeccion04/01-04-objetos.js
+++ JavaScript/TSLeccion04/01-04-objetos.js
@@ -3,118 +3,67 @@ console.log(x.length);
 
 //Objeto
 let persona = {
-    nombre: "Carlos",
-    apellido: "Gil",
-    email: "cgil@gmail.com",
-    edad: 30
-}
+  nombre: "Carlos",
+  apellido: "Gil",
+  email: "cgil@gmail.com",
+  edad: 30,
+};
 
 console.log(persona.nombre);
 console.log(persona.apellido);
 console.log(persona.email);
 console.log(persona.edad);
-console.log(persona)
+console.log(persona);
 
 //AGREGAR O ELIMINAR PROPIEDADES DE LOS OBJETOS
-persona.apellida = 'Betancud'; //Cambiamos dinamicamente el valor de un objeto
+persona.apellida = "Betancud"; //Cambiamos dinamicamente el valor de un objeto
 delete persona.apellida; //Eliminamos el error
 console.log(persona);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //Distintas formas de imprimir un objeto
 //Numero 1: la mas sencilla, concatenar cada valor de cada propiedad
-console.log("Distintas formas de imprimir un objeto: forma 1")
-console.log(persona.nombre + ', ' + persona.apellido)
+console.log("Distintas formas de imprimir un objeto: forma 1");
+console.log(persona.nombre + ", " + persona.apellido);
 
 //Numero 2: A traves del ciclo for in
-console.log("Distintas formas de imprimir un objeto: forma 2")
-for(nombrePropiedad in persona){
-    console.log(persona[nombrePropiedad])
+console.log("Distintas formas de imprimir un objeto: forma 2");
+for (nombrePropiedad in persona) {
+  console.log(persona[nombrePropiedad]);
 }
 
 //Numero 3: La funcion Object.value()
-console.log("Distintas formas de imprimir un objeto: forma 3")
+console.log("Distintas formas de imprimir un objeto: forma 3");
 let personaArray = Object.values(persona);
-console.log(personaArray)
+console.log(personaArray);
 
 //Numero 4: Usamos el metodo JSON.stringify
-console.log("Distintas formas de imprimir un objeto: forma 4")
+console.log("Distintas formas de imprimir un objeto: forma 4");
 let personaString = JSON.stringify(persona);
-console.log(personaString)
-
-
-
-
-
-
-
-
-
+console.log(personaString);
 
 //VIDEO 3: CONSTRUCTORES DE OBJETOS(CLASE 5){
 
-
-            
-            
 //Agregamos Métodos al constructor del objeto(Video 4)
-    this.nombreCompleto = function(){
-        return this.nombre+' '+this.apellido;
-    }
-}
-
- 
-
-
-
-
-
-
-
+this.nombreCompleto = function () {
+  return this.nombre + " " + this.apellido;
+};
 
 //Mostramos el objeto padre utilizando la funcion (Video 4)
 console.log(padre.nombreCompleto()); //Utilizamos la funcion
-
-
-
-
-
-
-
-
-
 
 //Mostramos objeto madre utilizando la funcion(video 4)
 console.log(madre.nombreCompleto());
 
 //Diferentes formas de crear objetos
 //Caso numero 1
-let miObjeto = new Object() //Esta es una opcion formal
+let miObjeto = new Object(); //Esta es una opcion formal
 //Caso numero 2
 let miObjeto2 = {}; //Esta es una opcion breve y recomendada
 
 //Caso string 1
-let miCadena1 = new String('Hola');//Sintaxis formal
-//Caso string 2 
-let miCadena2 = 'Hola' //Esta es la sintaxis simplificada y recomendada
+let miCadena1 = new String("Hola"); //Sintaxis formal
+//Caso string 2
+let miCadena2 = "Hola"; //Esta es la sintaxis simplificada y recomendada
 
 //Caso con numeros 1
 let miNumero = new Number(1); //Sintaxis formal no recomendable
@@ -132,42 +81,30 @@ let miArreglo1 = new Array(); //Formal
 let miArreglo2 = []; //Sintaxis recomendada
 
 //Caso function 1
-let miFuncion1 = new function(){}; //Todo despues de new es considerado objeto
+let miFuncion1 = new (function () {})(); //Todo despues de new es considerado objeto
 //Caso function 2
-let miFuncion2 = function(){}; //Sintaxis simplificada y recomendada
-
-
-
-
-
-
-
-
-
-
+let miFuncion2 = function () {}; //Sintaxis simplificada y recomendada
 
 //Uso de call
 let persona4 = {
-
-    nombre: 'Juan',
-    apellida: 'Perez',
-    nombreCompleto2: function(titulo, telefono){
-        return titulo+': '+this.nombre+' '+this.apellido+' '+telefono;
-        //return this.nombre+' '+this.apellido;
-    }
-}
+  nombre: "Juan",
+  apellida: "Perez",
+  nombreCompleto2: function (titulo, telefono) {
+    return titulo + ": " + this.nombre + " " + this.apellido + " " + telefono;
+    //return this.nombre+' '+this.apellido;
+  },
+};
 
 let persona5 = {
-    nombre: "Carlos",
-    apellido: "Lara"
-}
+  nombre: "Carlos",
+  apellido: "Lara",
+};
 
-console.log(persona4.nombreCompleto2('Lic', '1321312312312'))
-console.log(persona4.nombreCompleto2.call(persona5, 'Ing', '123124543534'))
+console.log(persona4.nombreCompleto2("Lic", "1321312312312"));
+console.log(persona4.nombreCompleto2.call(persona5, "Ing", "123124543534"));
 
-console.log(persona4.nombreCompleto2('Lic.', '2635484545'));
-
+console.log(persona4.nombreCompleto2("Lic.", "2635484545"));
 
 //Metodo Apply
-let arreglo =['Ing.', '547895662'];
+let arreglo = ["Ing.", "547895662"];
 console.log(persona4.nombreCompleto2.apply(persona5, arreglo));
