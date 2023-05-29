94dabd095143f922df3d05791603f3a30a4faa88
Agustin Vera <ronedev25@gmail.com>
2023-05-29 14:46:39 -0300

    add: Manejo de logging parte 1

---
+++ Python/Leccion07/capa_datos_persona/logger_base.py
@@ -0,0 +1,10 @@
+import logging as log
+
+#Llamamos una configuracion basica
+if __name__ == '__main__':
+    log.basicConfig(level=log.DEBUG)
+    log.debug('Mensaje a nivel debug')
+    log.info('Mensaje a nivel info')
+    log.warning('Mensaje a nivel warning')
+    log.error('Mensaje a nivel error')
+    log.critical('Mensaje a nivel critical')
