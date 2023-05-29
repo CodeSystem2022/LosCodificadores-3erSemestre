398b09adf8a22a906e3b84061643e186120b7e95
Agustin Vera <ronedev25@gmail.com>
2023-05-29 14:33:09 -0300

    add: metodos set a la clase persona

--- JavaScript/TSLeccion05/05-03-clases.js
+++ JavaScript/TSLeccion05/05-03-clases.js
@@ -3,12 +3,40 @@ class Persona{ //Clase padre
         this._nombre = nombre;
         this._apellido = apellido;
     }
+
+    get nombre(){
+        return this._nombre;
+    }
+
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+
+    get apellido(){
+        return this._apellido
+    }
+
+    set apellido(apellido){
+        this._apellido = apellido;
+    }
 }
 
 let persona1 = new Persona('Martin', 'Perez');
-console.log(persona1);
+console.log(persona1.nombre);
+persona1.nombre = 'Juan Carlos'
+console.log(persona1.nombre);
+console.log(persona1.apellido);
+persona1.apellido = 'Gomez'
+console.log(persona1.apellido);
+//console.log(persona1);
 let persona2 = new Persona('Carlos', 'Lara');
-console.log(persona2);
+console.log(persona2.nombre);
+persona2.nombre = 'Maria Laura'
+console.log(persona2.nombre);
+console.log(persona2.apellido);
+persona2.apellido = 'Rodriguez'
+console.log(persona2.apellido);
+//console.log(persona2);
 
 
 
@@ -23,7 +51,7 @@ console.log(persona2);
 
 
 nombreCompleto(){
-    return this._nombre+' '+this._apellido;
+   return this._nombre+' '+this._apellido;
 }
 
 }
