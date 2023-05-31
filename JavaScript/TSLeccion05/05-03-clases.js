fa23e93fbdb92685682adf3575d9c864d554714a
Nicolas Aranibar <aranibarnicolas596@gmail.com>
2023-05-31 01:40:41 -0300

    Herencia parte 2

--- JavaScript/TSLeccion05/05-03-clases.js
+++ JavaScript/TSLeccion05/05-03-clases.js
@@ -1,45 +1,14 @@
-
-// let persona3 = new Persona('Carla', 'Ponce'); esto no se debe hacer: Persona is not defined.
-
 class Persona{ //Clase padre
     constructor(nombre, apellido){
         this._nombre = nombre;
         this._apellido = apellido;
     }
-
-    get nombre(){
-        return this._nombre;
-    }
-
-    set nombre(nombre){
-        this._nombre = nombre;
-    }
-
-    get apellido(){
-        return this._apellido
-    }
-
-    set apellido(apellido){
-        this._apellido = apellido;
-    }
 }
 
 let persona1 = new Persona('Martin', 'Perez');
-console.log(persona1.nombre);
-persona1.nombre = 'Juan Carlos'
-console.log(persona1.nombre);
-console.log(persona1.apellido);
-persona1.apellido = 'Gomez'
-console.log(persona1.apellido);
-//console.log(persona1);
+console.log(persona1);
 let persona2 = new Persona('Carlos', 'Lara');
-console.log(persona2.nombre);
-persona2.nombre = 'Maria Laura'
-console.log(persona2.nombre);
-console.log(persona2.apellido);
-persona2.apellido = 'Rodriguez'
-console.log(persona2.apellido);
-//console.log(persona2);
+console.log(persona2);
 
 
 
@@ -53,16 +22,9 @@ console.log(persona2.apellido);
 
 
 
-nombreCompleto(){
-   return this._nombre+' '+this._apellido;
+nombreCompleto();{
+    return this._nombre+' '+this._apellido;
 }
-
-}
-
-
-
-
-
 
 
 
@@ -90,7 +52,8 @@ console.log(empleado1.nombreCompleto());
 
 
 class Empleado extends Persona{ //Clase hija
-    constructor(departamento){
+    constructor(nombre, apellido, departamento){
+        super(nombre, apellido);
         this._departamento = departamento;
     }
     get departamento(){
@@ -100,3 +63,6 @@ class Empleado extends Persona{ //Clase hija
         this._departamento = departamento;
     }
 }
+
+let empleado1 = new Empleado('Maria', 'Gimenez', 'Sistemas');
+console.log(empleado1);
