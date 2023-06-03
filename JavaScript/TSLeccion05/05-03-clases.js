5968d69a645d8d2296c080252ef03cbb33d69f3b
Agustin Vera <ronedev25@gmail.com>
2023-06-03 10:49:57 -0300

    add: atributos estaticos en la clase Persona

--- JavaScript/TSLeccion05/05-03-clases.js
+++ JavaScript/TSLeccion05/05-03-clases.js
@@ -1,7 +1,12 @@
 class Persona{ //Clase padre
+
+    static contadorObjetosPersona = 0;
+
     constructor(nombre, apellido){
         this._nombre = nombre;
         this._apellido = apellido;
+        Persona.contadorObjetosPersona++;
+        console.log('Se incrementa el contador: '+Persona.contadorObjetosPersona)
     }
     get nombre(){
         return this._nombre;
@@ -70,3 +75,24 @@ Persona.saludar2(persona1);
 
 Empleado.saludar();
 Empleado.saludar2(empleado1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(persona1.contadorObjetosPersona)
+console.log(Persona.contadorObjetosPersona)
+console.log(Empleado.contadorObjetosPersona)
