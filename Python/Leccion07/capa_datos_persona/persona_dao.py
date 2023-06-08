82eea080a35032e00aab722848d1830222c8bce9
Cecilia Iribarren <cecirib@gmail.com>
2023-06-08 18:23:44 -0300

    Add: Change: pequeños cambios al código

--- Python/Leccion07/capa_datos_persona/persona_dao.py
+++ Python/Leccion07/capa_datos_persona/persona_dao.py
@@ -11,4 +11,3 @@ class PersonaDAO:
     _INSERTAR = 'INSERT INTO persona(nombre, apellido, email)VALUES (%s, %s, %s)'
     _ACTUALIZAR = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
     _ELIMINAR = 'DELETE FROM persona WHERE id_persona=%s'
-    
