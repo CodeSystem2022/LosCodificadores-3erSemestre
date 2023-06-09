4ff57ae605187dcaeaf4ee604420d1408ad430b0
Agustin Vera <ronedev25@gmail.com>
2023-06-09 14:46:52 -0300

    add: Metodo insertar

--- Python/Leccion07/capa_datos_persona/persona_dao.py
+++ Python/Leccion07/capa_datos_persona/persona_dao.py
@@ -29,7 +29,21 @@ class PersonaDAO:
                     persona.append(persona)
                 return personas
 
+    @classmethod
+    def insertar(cls, persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona.nombre, persona.apellido, persona.email)
+                cursor.execute(cls._INSERTAR, valores)
+                log.debug(f'Persona Insertada: {persona}')
+                return cursor.rowcount
+
 if __name__ == '__main__':
+    #Insertar registro
+    persona1 = Persona(nombre="Pedro", apellido="Romero", email="promero@mail.com")+
+    personas_insertadas = PersonaDAO.insertar(persona1)
+    log.debug(f'Personas Insertadas: {personas_insertadas}')
+    #Seleccionar objetos
     personas = PersonaDAO.seleccionar()
     for persona in personas:
         log.debug(persona)
