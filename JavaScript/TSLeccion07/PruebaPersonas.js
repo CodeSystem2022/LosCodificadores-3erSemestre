720d9c8aa6a971ad5043d7053c503bc8f0159936
Nicolas Aranibar <aranibarnicolas596@gmail.com>
2023-06-13 19:18:54 -0300

    Prueba clase cliente

--- JavaScript/TSLeccion07/PruebaPersonas.js
+++ JavaScript/TSLeccion07/PruebaPersonas.js
@@ -92,10 +92,7 @@ class Cliente extends Persona{
     }
 
     toString(){
-        return `
-        ${super.toString()} 
-        ${this._idCliente} 
-        ${this._fechaRegistro}`;
+        return `${super.toString()} ${this._idCliente} ${this._fechaRegistro}`;
     }
 }
 
@@ -112,4 +109,8 @@ console.log(empleado1.toString())
 
 let empleado2 = new Empleado('Jonas', 'Torres', 30, 7000)
 console.log(empleado2.toString())
+
+//Prueba clase cliente
+let cliente1 = new Cliente('Miguel', 'Zala', 29, new Date());
+console.log(cliente1.toString());
 
