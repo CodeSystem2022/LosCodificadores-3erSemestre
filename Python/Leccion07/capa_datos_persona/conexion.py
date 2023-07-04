26fa5a7bf506dea31d44113af5ddf69590e87067
Cecilia Iribarren <cecirib@gmail.com>
2023-07-04 13:22:39 -0300

    Add: Agregamos Pool de conexiones

--- Python/Leccion07/capa_datos_persona/conexion.py
+++ Python/Leccion07/capa_datos_persona/conexion.py
@@ -1,4 +1,4 @@
-import psycopg2 as bd
+from psycopg2 import pool
 # psicopg2 as db otra manera de importar psycopg2
 from logger_base import log
 import sys
@@ -10,38 +10,17 @@ class Conexion:
     _PASSWORD = 'admin'
     _DB_PORT = '5432'
     _HOST = '127.0.0.1'
-    _conexion = None
-    _cursor = None
+    _MIN_CON = 1
+    _MAX_CON = 5
+    _pool = None
 
     @classmethod
     def obtenerConexion(cls):
-        if cls._conexion is None:
-            try:
-                cls._conexion = bd.connect(host= cls._HOST,
-                                           user= cls._USERNAME,
-                                           password= cls._PASSWORD,
-                                           port= cls._DB_PORT,
-                                           database= cls._DATABASE)
-                log.debug(f'Conexión exitosa: {cls._conexion}')
-                return cls._conexion
-            except Exception as e:
-                log.error(f'Ocurrio un error: {e}')
-                sys.exit()
-        else:
-            return cls._conexion
-        
+        pass
+
     @classmethod
     def obtenerCursor(cls):
-        if cls._cursor is None:
-            try:
-                cls._cursor = cls.obtenerConexion().cursor()
-                log.debug(f'Se abrió correctamente el cursor: {cls._cursor}')
-                return cls._cursor
-            except Exception as e:
-                log.error(f'Ocurrió un error: {e}')
-                sys.exit()
-            else: 
-                return cls._cursor
+        pass
 
     @classmethod
     def obtenerPool(cls):
@@ -63,6 +42,4 @@ class Conexion:
             return cls._pool
             
 if __name__ == '__main__':
-    Conexion.obtenerConexion()
-    Conexion.obtenerCursor()
-            
+    pass
