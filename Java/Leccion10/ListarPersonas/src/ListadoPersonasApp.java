038aaa0fd208d8d09046a4f9e3ddece364748dc8
GuillermoEQ <qguillermoe@gmail.com>
2023-07-10 10:52:22 -0300

    add: Se agrgó el try catch y el método ejecutarOperacion.

--- Java/Leccion10/ListarPersonas/src/ListadoPersonasApp.java
+++ Java/Leccion10/ListarPersonas/src/ListadoPersonasApp.java
@@ -2,6 +2,7 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
 public class ListadoPersonasApp {
+
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         //Definimos la lista fuera del while
@@ -10,6 +11,11 @@ public class ListadoPersonasApp {
         var salir = false;
         while(!salir){
             mostrarMenu();
+            try{
+                salir = ejecutarOperacion(entrada, personas);
+            } catch(Exception e){
+                System.out.println("Ocurrió un error: "+e.getMessage());
+            }
             System.out.println();
         }//Fin ciclo while
     }//Fin metodo main
@@ -24,4 +30,25 @@ public class ListadoPersonasApp {
                 """);
         System.out.print("Digite una de las opciones: ");
     }
-}
+
+    private static boolean ejecutarOperacion(Scanner entrada, List<Persona> personas){
+        var opcion = Integer.parseInt(entrada.nextLine());
+        var salir = false;
+        // Revismos la opción digitada a través de un switch
+        switch (opcion){
+            case 1 -> { //Agregar una persona a la lista
+                System.out.print("Digite el nombre: ");
+                var nombre = entrada.nextLine();
+                System.out.print("Digite el telefono: ");
+                var tel = entrada.nextLine();
+                System.out.print("Digite el correo: ");
+                var email = entrada.nextLine();
+                // Se crea el objeto persona
+                var persona = new Persona(nombre, tel, email);
+                // Agregamos la persona a la lista
+                personas.add(persona);
+                System.out.println("la lista tiene: "+personas.size()+" elementos");
+            } // Fin caso 1
+        }
+    } // Fin del método ejecutarOperación
+} // Fin de la clase ListadoPersonasApp
