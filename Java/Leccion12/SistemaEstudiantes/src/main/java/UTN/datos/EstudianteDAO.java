5782eac31d151fb149e4d7c6adbc2860f8d52012
GuillermoEQ <qguillermoe@gmail.com>
2023-07-11 14:59:13 -0300

    add: Se agrega el método modificarEstudiante.

--- Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
+++ Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
@@ -75,40 +75,6 @@ public class EstudianteDAO {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     //Metodo agregar un nuevo estudiante
     public boolean agregarEstudiante(Estudiante estudiante){
         PreparedStatement ps;
@@ -134,6 +100,33 @@ public class EstudianteDAO {
         }//Fin finally
         return false;
     }//Fin metodo agregarEstudiante
+
+    // Método para modificar estudiante
+    public boolean modificarEstudiante(Estudiante estudiante){
+        PreparedStatement ps;
+        Connection con = getConnection();
+        String sql = "UPDATE estudiantes2022 SET nombre=?, apellido=?, telefono=?, email=? WHERE idestudiantes2022=?";
+        try{
+            ps = con.prepareStatement(sql);
+            ps.setString(1, estudiante.getNombre());
+            ps.setString(2, estudiante.getApellido());
+            ps.setString(3, estudiante.getTelefono());
+            ps.setString(4, estudiante.getEmail());
+            ps.setInt(5, estudiante.getIdEstudiante());
+            ps.execute();
+            return true;
+        } catch (Exception e){
+            System.out.println("Error al modificar estudiante: "+e.getMessage());
+        } // Fin catch
+        finally {
+            try {
+                con.close();
+            } catch (Exception e){
+                System.out.println("Error al cerrar la conexión: "+e.getMessage());
+            } // Fin catch
+        } // Fin finally
+        return false;
+    } // Fin método modificarEstudiante
 
     public static void main(String[] args){
         //Listar los estudiantes
